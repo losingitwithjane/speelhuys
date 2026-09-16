f546b4d2704377478bd99b32cac83dc64b47ef7d
lukasdetweede <zwxylukas@gmail.com>
2026-09-16 13:24:54 +0200

    delete voor thema en merk

---
+++ admin/themeDelete.php
@@ -0,0 +1,73 @@
+<?php
+include "../classes/database.php";
+include "../classes/sessie.php";
+include "../classes/gebruiker.php";
+include "../classes/theme.php";
+
+if (!isset($_GET["id"])) {
+    header("Location: ../productpagina.php?message=Geen thema ID opgegeven.");
+    exit;
+}
+
+if (!isset($_COOKIE["speelhuys-session"])) {
+    header("Location: ../index.php?message=Geen cookie.");
+}
+
+$id = $_GET["id"];
+
+$conn = Database::start();
+$session = Sessie::findSession();
+$user = User::findById($session->session_user_id);
+$theme = Theme::findById($id);
+
+if ($session == null) {
+    header("Location: ../index.php?message=Geen actieve sessie.");
+    exit;
+}
+
+if ($user->rol != "admin"){    
+    header("Location: admin.php?message=Geen toestemming.");
+    exit;
+}
+
+if ($theme == null) {
+    header("Location: admin.php?message=Thema niet gevonden.");
+    exit;
+}
+
+// Verwijder na bevestiging
+if (isset($_POST["ja"])) {
+    $theme->delete();
+    header("Location: admin.php?message=Thema succesvol verwijderd.");
+    exit;
+}
+
+if (isset($_POST["nee"])) {
+    header("Location: admin.php?message=Verwijderen geannuleerd.");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Set verwijderen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container text-center mt-5" style="max-width: 400px;">
+        <div class="card text-bg-dark mb-3">
+            <div class="card-body">
+                <h3 class="card-title">Verwijderen?</h3>
+                <p class="card-text">
+                <form method="post">
+                    <button class="btn btn-danger" type="submit" name="ja">Ja</button>
+                    <button class="btn btn-secondary" type="submit" name="nee">Nee</button>
+                </form>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
